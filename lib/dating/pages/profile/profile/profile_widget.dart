23a39817ade63ef10275bfecbeb9e1a76f8e091c
FlutterFlow <140657486+FlutterFlowEng@users.noreply.github.com>
2025-03-11 20:04:02 +0000

    Updating to latest FlutterFlow output.

--- lib/dating/pages/profile/profile/profile_widget.dart
+++ lib/dating/pages/profile/profile/profile_widget.dart
@@ -79,7 +79,7 @@ class _ProfileWidgetState extends State<ProfileWidget> {
                         size: 24.0,
                       ),
                       onPressed: () async {
-                        setDarkModeSetting(context, ThemeMode.dark);
+                        setDarkModeSetting(context, ThemeMode.light);
                       },
                     ),
                   if (Theme.of(context).brightness == Brightness.light)
@@ -92,7 +92,7 @@ class _ProfileWidgetState extends State<ProfileWidget> {
                         size: 24.0,
                       ),
                       onPressed: () async {
-                        setDarkModeSetting(context, ThemeMode.light);
+                        setDarkModeSetting(context, ThemeMode.dark);
                       },
                     ),
                 ],
@@ -104,204 +104,209 @@ class _ProfileWidgetState extends State<ProfileWidget> {
         ),
         body: SafeArea(
           top: true,
-          child: Column(
-            mainAxisSize: MainAxisSize.max,
-            children: [
-              AuthUserStreamWidget(
-                builder: (context) => wrapWithModel(
-                  model: _model.profileImageWidgetModel,
-                  updateCallback: () => safeSetState(() {}),
-                  child: ProfileImageWidgetWidget(
-                    height: 200.0,
-                    width: 200.0,
-                    isEdit: false,
-                    imageLink: valueOrDefault<String>(
-                      (currentUserDocument?.pictures.toList() ?? [])
-                          .lastOrNull
-                          ?.imageLink,
-                      'https://cdn.pixabay.com/photo/2023/02/18/11/00/icon-7797704_960_720.png',
-                    ),
-                    blurHash: valueOrDefault<String>(
-                      (currentUserDocument?.pictures.toList() ?? [])
-                          .lastOrNull
-                          ?.hash,
-                      'L6Pj0^jE.AyE_3t7t7R**0o#DgR4',
+          child: SingleChildScrollView(
+            child: Column(
+              mainAxisSize: MainAxisSize.max,
+              children: [
+                AuthUserStreamWidget(
+                  builder: (context) => wrapWithModel(
+                    model: _model.profileImageWidgetModel,
+                    updateCallback: () => safeSetState(() {}),
+                    child: ProfileImageWidgetWidget(
+                      height: 200.0,
+                      width: 200.0,
+                      isEdit: false,
+                      imageLink: valueOrDefault<String>(
+                        (currentUserDocument?.pictures.toList() ?? [])
+                            .lastOrNull
+                            ?.imageLink,
+                        'https://cdn.pixabay.com/photo/2023/02/18/11/00/icon-7797704_960_720.png',
+                      ),
+                      blurHash: valueOrDefault<String>(
+                        (currentUserDocument?.pictures.toList() ?? [])
+                            .lastOrNull
+                            ?.hash,
+                        'L6Pj0^jE.AyE_3t7t7R**0o#DgR4',
+                      ),
                     ),
                   ),
                 ),
-              ),
-              Align(
-                alignment: AlignmentDirectional(0.0, 0.0),
-                child: Column(
-                  mainAxisSize: MainAxisSize.min,
-                  children: [
-                    AuthUserStreamWidget(
-                      builder: (context) => Text(
-                        valueOrDefault<String>(
-                          currentUserDisplayName,
-                          'Name',
-                        ),
-                        style: FlutterFlowTheme.of(context).titleLarge.override(
-                              fontFamily: 'Plus Jakarta Sans',
-                              letterSpacing: 0.0,
-                            ),
-                      ),
-                    ),
-                    Text(
-                      valueOrDefault<String>(
-                        currentUserEmail,
-                        'test@mail.com',
-                      ),
-                      style: FlutterFlowTheme.of(context).labelMedium.override(
-                            fontFamily: 'Space Grotesk',
-                            letterSpacing: 0.0,
+                Align(
+                  alignment: AlignmentDirectional(0.0, 0.0),
+                  child: Column(
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      AuthUserStreamWidget(
+                        builder: (context) => Text(
+                          valueOrDefault<String>(
+                            currentUserDisplayName,
+                            'Name',
                           ),
-                    ),
-                  ].divide(SizedBox(height: 8.0)),
-                ),
-              ),
-              Expanded(
-                child: Column(
-                  mainAxisSize: MainAxisSize.max,
-                  children: [
-                    Padding(
-                      padding: EdgeInsets.all(16.0),
-                      child: InkWell(
-                        splashColor: Colors.transparent,
-                        focusColor: Colors.transparent,
-                        hoverColor: Colors.transparent,
-                        highlightColor: Colors.transparent,
-                        onTap: () async {
-                          context.pushNamed(EditProfileWidget.routeName);
-                        },
-                        child: Row(
-                          mainAxisSize: MainAxisSize.max,
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          children: [
-                            Row(
-                              mainAxisSize: MainAxisSize.min,
-                              children: [
-                                FaIcon(
-                                  FontAwesomeIcons.userCircle,
-                                  color:
-                                      FlutterFlowTheme.of(context).primaryText,
-                                  size: 24.0,
-                                ),
-                                Text(
-                                  FFLocalizations.of(context).getText(
-                                    'ymu7av9g' /* Personal Detail */,
+                          style:
+                              FlutterFlowTheme.of(context).titleLarge.override(
+                                    fontFamily: 'Plus Jakarta Sans',
+                                    letterSpacing: 0.0,
                                   ),
-                                  style: FlutterFlowTheme.of(context)
-                                      .bodyLarge
-                                      .override(
-                                        fontFamily: 'Space Grotesk',
-                                        letterSpacing: 0.0,
-                                      ),
-                                ),
-                              ].divide(SizedBox(width: 16.0)),
-                            ),
-                            Icon(
-                              Icons.play_arrow_rounded,
-                              color: FlutterFlowTheme.of(context).primaryText,
-                              size: 24.0,
-                            ),
-                          ],
                         ),
                       ),
-                    ),
-                    if (!valueOrDefault<bool>(
-                        currentUserDocument?.isPremium, false))
+                      Text(
+                        valueOrDefault<String>(
+                          currentUserEmail,
+                          'test@mail.com',
+                        ),
+                        style:
+                            FlutterFlowTheme.of(context).labelMedium.override(
+                                  fontFamily: 'Space Grotesk',
+                                  letterSpacing: 0.0,
+                                ),
+                      ),
+                    ].divide(SizedBox(height: 8.0)),
+                  ),
+                ),
+                Expanded(
+                  child: Column(
+                    mainAxisSize: MainAxisSize.max,
+                    children: [
                       Padding(
                         padding: EdgeInsets.all(16.0),
-                        child: AuthUserStreamWidget(
-                          builder: (context) => InkWell(
-                            splashColor: Colors.transparent,
-                            focusColor: Colors.transparent,
-                            hoverColor: Colors.transparent,
-                            highlightColor: Colors.transparent,
-                            onTap: () async {
-                              context.pushNamed(SubscriptionWidget.routeName);
-                            },
-                            child: Row(
-                              mainAxisSize: MainAxisSize.max,
-                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                              children: [
-                                Row(
-                                  mainAxisSize: MainAxisSize.min,
-                                  children: [
-                                    Icon(
-                                      Icons.workspace_premium_rounded,
-                                      color: FlutterFlowTheme.of(context)
-                                          .primaryText,
-                                      size: 24.0,
+                        child: InkWell(
+                          splashColor: Colors.transparent,
+                          focusColor: Colors.transparent,
+                          hoverColor: Colors.transparent,
+                          highlightColor: Colors.transparent,
+                          onTap: () async {
+                            context.pushNamed(EditProfileWidget.routeName);
+                          },
+                          child: Row(
+                            mainAxisSize: MainAxisSize.max,
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              Row(
+                                mainAxisSize: MainAxisSize.min,
+                                children: [
+                                  FaIcon(
+                                    FontAwesomeIcons.userCircle,
+                                    color: FlutterFlowTheme.of(context)
+                                        .primaryText,
+                                    size: 24.0,
+                                  ),
+                                  Text(
+                                    FFLocalizations.of(context).getText(
+                                      'ymu7av9g' /* Personal Detail */,
                                     ),
-                                    Text(
-                                      FFLocalizations.of(context).getText(
-                                        '434adhr3' /* Premium */,
-                                      ),
-                                      style: FlutterFlowTheme.of(context)
-                                          .bodyLarge
-                                          .override(
-                                            fontFamily: 'Space Grotesk',
-                                            letterSpacing: 0.0,
-                                          ),
-                                    ),
-                                  ].divide(SizedBox(width: 16.0)),
-                                ),
-                                Icon(
-                                  Icons.play_arrow_rounded,
-                                  color:
-                                      FlutterFlowTheme.of(context).primaryText,
-                                  size: 24.0,
-                                ),
-                              ],
-                            ),
+                                    style: FlutterFlowTheme.of(context)
+                                        .bodyLarge
+                                        .override(
+                                          fontFamily: 'Space Grotesk',
+                                          letterSpacing: 0.0,
+                                        ),
+                                  ),
+                                ].divide(SizedBox(width: 16.0)),
+                              ),
+                              Icon(
+                                Icons.play_arrow_rounded,
+                                color: FlutterFlowTheme.of(context).primaryText,
+                                size: 24.0,
+                              ),
+                            ],
                           ),
                         ),
                       ),
-                  ],
-                ),
-              ),
-              FlutterFlowLanguageSelector(
-                width: 200.0,
-                height: 40.0,
-                borderColor: Colors.transparent,
-                dropdownIconColor: FlutterFlowTheme.of(context).secondaryText,
-                borderRadius: 8.0,
-                textStyle: FlutterFlowTheme.of(context).bodyMedium.override(
-                      fontFamily: 'Space Grotesk',
-                      letterSpacing: 0.0,
-                    ),
-                hideFlags: false,
-                flagSize: 24.0,
-                flagTextGap: 8.0,
-                currentLanguage: FFLocalizations.of(context).languageCode,
-                languages: FFLocalizations.languages(),
-                onChanged: (lang) => setAppLanguage(context, lang),
-              ),
-              Padding(
-                padding: EdgeInsetsDirectional.fromSTEB(16.0, 0.0, 16.0, 0.0),
-                child: wrapWithModel(
-                  model: _model.mainButtonFillWidgetModel,
-                  updateCallback: () => safeSetState(() {}),
-                  child: MainButtonFillWidgetWidget(
-                    title: 'Logout',
-                    isDisable: false,
-                    onTap: () async {
-                      GoRouter.of(context).prepareAuthEvent();
-                      await authManager.signOut();
-                      GoRouter.of(context).clearRedirectLocation();
-
-                      context.goNamedAuth(
-                          OnboardingWidget.routeName, context.mounted);
-                    },
+                      if (!valueOrDefault<bool>(
+                          currentUserDocument?.isPremium, false))
+                        Padding(
+                          padding: EdgeInsets.all(16.0),
+                          child: AuthUserStreamWidget(
+                            builder: (context) => InkWell(
+                              splashColor: Colors.transparent,
+                              focusColor: Colors.transparent,
+                              hoverColor: Colors.transparent,
+                              highlightColor: Colors.transparent,
+                              onTap: () async {
+                                context.pushNamed(SubscriptionWidget.routeName);
+                              },
+                              child: Row(
+                                mainAxisSize: MainAxisSize.max,
+                                mainAxisAlignment:
+                                    MainAxisAlignment.spaceBetween,
+                                children: [
+                                  Row(
+                                    mainAxisSize: MainAxisSize.min,
+                                    children: [
+                                      Icon(
+                                        Icons.workspace_premium_rounded,
+                                        color: FlutterFlowTheme.of(context)
+                                            .primaryText,
+                                        size: 24.0,
+                                      ),
+                                      Text(
+                                        FFLocalizations.of(context).getText(
+                                          '434adhr3' /* Premium */,
+                                        ),
+                                        style: FlutterFlowTheme.of(context)
+                                            .bodyLarge
+                                            .override(
+                                              fontFamily: 'Space Grotesk',
+                                              letterSpacing: 0.0,
+                                            ),
+                                      ),
+                                    ].divide(SizedBox(width: 16.0)),
+                                  ),
+                                  Icon(
+                                    Icons.play_arrow_rounded,
+                                    color: FlutterFlowTheme.of(context)
+                                        .primaryText,
+                                    size: 24.0,
+                                  ),
+                                ],
+                              ),
+                            ),
+                          ),
+                        ),
+                    ],
                   ),
                 ),
-              ),
-            ]
-                .divide(SizedBox(height: 16.0))
-                .addToStart(SizedBox(height: 12.0))
-                .addToEnd(SizedBox(height: 70.0)),
+                FlutterFlowLanguageSelector(
+                  width: 200.0,
+                  height: 40.0,
+                  borderColor: Colors.transparent,
+                  dropdownIconColor: FlutterFlowTheme.of(context).secondaryText,
+                  borderRadius: 8.0,
+                  textStyle: FlutterFlowTheme.of(context).bodyMedium.override(
+                        fontFamily: 'Space Grotesk',
+                        letterSpacing: 0.0,
+                      ),
+                  hideFlags: false,
+                  flagSize: 24.0,
+                  flagTextGap: 8.0,
+                  currentLanguage: FFLocalizations.of(context).languageCode,
+                  languages: FFLocalizations.languages(),
+                  onChanged: (lang) => setAppLanguage(context, lang),
+                ),
+                Padding(
+                  padding: EdgeInsetsDirectional.fromSTEB(16.0, 0.0, 16.0, 0.0),
+                  child: wrapWithModel(
+                    model: _model.mainButtonFillWidgetModel,
+                    updateCallback: () => safeSetState(() {}),
+                    child: MainButtonFillWidgetWidget(
+                      title: 'Logout',
+                      isDisable: false,
+                      onTap: () async {
+                        GoRouter.of(context).prepareAuthEvent();
+                        await authManager.signOut();
+                        GoRouter.of(context).clearRedirectLocation();
+
+                        context.goNamedAuth(
+                            OnboardingWidget.routeName, context.mounted);
+                      },
+                    ),
+                  ),
+                ),
+              ]
+                  .divide(SizedBox(height: 16.0))
+                  .addToStart(SizedBox(height: 12.0))
+                  .addToEnd(SizedBox(height: 70.0)),
+            ),
           ),
         ),
       ),
